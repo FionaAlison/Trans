abbe4e6342b4e180b000ecedbad02524d9d34ec8
FionaAlison <47772002+FionaAlison@users.noreply.github.com>
2024-03-14 14:51:51 +0100

    Update meta_data.py

--- server/meta_data.py
+++ server/meta_data.py
@@ -1,29 +1,17 @@
-import requests
-metadata_url = "http://169.254.169.254/latest/meta-data/"
-# Function to fetch instance metadata
-def fetch_instance_metadata():
-    metadata_url = "http://169.254.169.254/latest/meta-data/"
-    try:
-        response = requests.get(metadata_url)
-        response.raise_for_status()
-        return response.text
-    except requests.exceptions.RequestException as e:
-        print("Error fetching instance metadata:", e)
-        return None
+import boto3
 
-# Get instance public DNS name and IP address
-metadata = fetch_instance_metadata()
-if metadata:
-    lines = metadata.split('\n')
-    host = requests.get(metadata_url + 'public-hostname').text
-    ip = requests.get(metadata_url + 'local-ipv4').text
-    print("Public DNS Name:", host)
-    print("Local IP Address:", ip)
-else:
-    # Fallback to static values
-    host = 'ec2-54-236-191-88.compute-1.amazonaws.com'
-    ip = '127.0.0.1'
+def fetch_instance_metadata(metadata_key):
+    ec2_client = boto3.client('ec2')
+    instance_id = ec2_client.instance_id
+    metadata = ec2_client.describe_instances(InstanceIds=[instance_id])['Reservations'][0]['Instances'][0]['Metadata'][metadata_key]
+    return metadata
 
+# Get instance public DNS name and IP address using boto3
+host = fetch_instance_metadata('public-hostname')
+ip = fetch_instance_metadata('local-ipv4')
+
+print("Public DNS Name:", public_hostname)
+print("Local IP Address:", local_ipv4)
 # Other static configurations
 port = 8080
 buffer_size = 1024
